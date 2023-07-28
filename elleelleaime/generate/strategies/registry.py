52c0fea7d1cca4d2f396a5a9ddb57242afe4c10a
André Silva <andre15andre@hotmail.com>
2023-07-28 20:52:32 +0200

    update alvis

--- elleelleaime/generate/strategies/registry.py
+++ elleelleaime/generate/strategies/registry.py
@@ -2,18 +2,18 @@ from generate.strategies.strategy import PatchGenerationStrategy
 from generate.strategies.models.openai import OpenAIChatCompletionModels
 from generate.strategies.models.alvis import AlvisHFModels
 
-class PatchGenerationStrategyRegistry():
+
+class PatchGenerationStrategyRegistry:
     """
     Class for storing and retrieving models based on their name.
     """
-    
+
     def __init__(self):
         self._models: dict[str, PatchGenerationStrategy] = {
             "gpt-3.5-turbo": OpenAIChatCompletionModels("gpt-3.5-turbo"),
-            "incoder-1B-alvis": AlvisHFModels("incoder-1B"),
-            "incoder-6B-alvis": AlvisHFModels("incoder-6B"),
+            "codegen-2b-multi": AlvisHFModels("Salesforce/codegen-2B-multi"),
         }
-        
+
     def get_generation(self, name: str) -> PatchGenerationStrategy:
         if name.lower().strip() not in self._models:
             raise ValueError(f"Unknown model {name}")
