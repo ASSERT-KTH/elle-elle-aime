407239a0be65b197973275ab08e916aacb5bc3bf
André Silva <andre15andre@hotmail.com>
2023-08-03 12:01:14 +0200

    add test for closure-101

--- elleelleaime/tests/sample/test_generate_samples.py
+++ elleelleaime/tests/sample/test_generate_samples.py
@@ -46,6 +46,47 @@ class TestClozeSamplesIncoder:
         assert sample["prompt"].count("<|mask:2|>") == 1
         assert sample["prompt"].count("<|mask:3|>") == 1
 
+    def test_closure_101(self):
+        bug = TestClozeSamplesIncoder.DEFECTS4J.get_bug("Closure-101")
+        assert bug is not None
+
+        sample = generate_sample(
+            bug=bug,
+            prompt_strategy=TestClozeSamplesIncoder.PROMPT_STRATEGY,
+            model_name=TestClozeSamplesIncoder.MODEL_NAME,
+        )
+
+        # Assert we are dealing with the correct bug and strategy
+        assert sample["identifier"] == "Closure-101"
+        assert sample["prompt_strategy"] == "zero-shot-cloze"
+
+        print(sample["buggy_code"])
+        print(sample["fixed_code"])
+
+        print(sample["prompt"])
+
+        # Assert that the buggy code and fixed code are properly separated
+        assert (
+            not "options.closurePass = flags.process_closure_primitives;"
+            in sample["buggy_code"]
+        )
+        assert (
+            "options.closurePass = flags.process_closure_primitives;"
+            in sample["fixed_code"]
+        )
+        assert "if (flags.process_closure_primitives) {" in sample["buggy_code"]
+        assert not "if (flags.process_closure_primitives) {" in sample["fixed_code"]
+
+        # Assert that the prompt is properly constructed
+        assert (
+            sample["prompt"]
+            .strip()
+            .startswith("protected CompilerOptions createOptions() {")
+        )
+        assert sample["prompt"].count("<|mask:") == 2
+        assert sample["prompt"].count("<|mask:0|>") == 1
+        assert sample["prompt"].count("<|mask:1|>") == 1
+
     def test_lang_10(self):
         bug = TestClozeSamplesIncoder.DEFECTS4J.get_bug("Lang-10")
         assert bug is not None
