eefa4766b7a71f333d58094292d30b1fa7c416f4
André Silva <andre15andre@hotmail.com>
2023-08-23 16:34:38 +0200

    update dependencies

--- elleelleaime/generate/resources/alvis/load_modules.sh
+++ elleelleaime/generate/resources/alvis/load_modules.sh
@@ -2,4 +2,3 @@ module purge  # Ensure we don't have any conflicting modules loaded
 
 module load Python/3.10.4-GCCcore-11.3.0
 module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
-module load JupyterLab/3.5.0-GCCcore-11.3.0
