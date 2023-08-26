f1eed3dc80ca170cdaef6d0f75032f57e3d4ab26
André Silva <andre15andre@hotmail.com>
2023-08-26 11:52:30 +0200

    fix instanciation

--- elleelleaime/generate/strategies/registry.py
+++ elleelleaime/generate/strategies/registry.py
@@ -9,6 +9,8 @@ from elleelleaime.generate.strategies.models.huggingface.incoder import (
     IncoderHFModels,
 )
 
+from typing import Tuple
+
 
 class PatchGenerationStrategyRegistry:
     """
@@ -16,13 +18,18 @@ class PatchGenerationStrategyRegistry:
     """
 
     def __init__(self, **kwargs):
-        self._models: dict[str, PatchGenerationStrategy] = {
-            "gpt-3.5-turbo": OpenAIChatCompletionModels("gpt-3.5-turbo", **kwargs),
-            "incoder-1b": IncoderHFModels("facebook/incoder-1B", **kwargs),
-            "incoder-6b": IncoderHFModels("facebook/incoder-6B", **kwargs),
+        # The registry is a dictionary of model names to a tuple of the model class and the model's constructor arguments
+        # NOTE: Do not instantiate the model here, as we should only instanciate the class to be used
+        self._models: dict[str, Tuple[type, Tuple]] = {
+            # OpenAI models
+            "gpt-3.5-turbo": (OpenAIChatCompletionModels, ("gpt-3.5-turbo", kwargs)),
+            # HuggingFace models
+            "incoder-1b": (IncoderHFModels, ("facebook/incoder-1B", kwargs)),
+            "incoder-6b": (IncoderHFModels, ("facebook/incoder-6B", kwargs)),
         }
 
     def get_generation(self, name: str) -> PatchGenerationStrategy:
         if name.lower().strip() not in self._models:
             raise ValueError(f"Unknown model {name}")
-        return self._models[name.lower().strip()]
+        model_class, model_args = self._models[name.lower().strip()]
+        return model_class(*model_args)
