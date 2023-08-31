c528dcfd1e2c6bae716994bd74035fd1eba1c329
André Silva <andre15andre@hotmail.com>
2023-08-31 11:03:17 +0200

    add logic for computing statistics and exporting patches

--- evaluate_patches.py
+++ evaluate_patches.py
@@ -8,6 +8,9 @@ import fire
 import sys
 import tqdm
 import logging
+import json
+import os
+import difflib
 
 
 def evaluate_candidate(bug: Bug, sample: dict, strategy: str, **kwargs) -> dict:
@@ -24,47 +27,190 @@ def evaluate_candidate(bug: Bug, sample: dict, strategy: str, **kwargs) -> dict:
     return sample
 
 
+def exact_match(evaluation: dict) -> bool:
+    """
+    Returns True if the evaluation is an exact match.
+    """
+    return evaluation["exact_match"]
+
+
+def plausible(evaluation: dict) -> bool:
+    """
+    Returns True if the evaluation is plausible.
+    """
+    return evaluation["compile"] and evaluation["test"]
+
+
+def compilable(evaluation: dict) -> bool:
+    """
+    Returns True if the evaluation is compilable.
+    """
+    return evaluation["compile"]
+
+
+def compute_statistics(samples: list) -> dict:
+    """
+    Computes statistics over the evaluation.
+    """
+    statistics = {
+        "num_bugs": 0,
+        "num_bugs_with_prompt": 0,
+        "num_bugs_with_candidates": 0,
+        "num_bugs_with_exact_match_candidates": 0,
+        "num_bugs_with_plausible_candidates": 0,
+        "num_bugs_with_compilable_candidates": 0,
+        "bugs_with_exact_match_candidates": [],
+        "bugs_with_plausible_candidates": [],
+        "bugs_with_compilable_candidates": [],
+    }
+
+    for sample in tqdm.tqdm(samples):
+        statistics["num_bugs"] += 1
+        if sample["prompt"] is not None:
+            statistics["num_bugs_with_prompt"] += 1
+        if (
+            sample["generation"] is not None
+            and len(sample["generation"]) > 0
+            and not all(
+                candidate["generation"] is None for candidate in sample["evaluation"]
+            )
+        ):
+            statistics["num_bugs_with_candidates"] += 1
+            if any(candidate["exact_match"] for candidate in sample["evaluation"]):
+                statistics["num_bugs_with_exact_match_candidates"] += 1
+                statistics["bugs_with_exact_match_candidates"].append(
+                    sample["identifier"]
+                )
+            if any(candidate["compile"] for candidate in sample["evaluation"]):
+                statistics["num_bugs_with_compilable_candidates"] += 1
+                statistics["bugs_with_compilable_candidates"].append(
+                    sample["identifier"]
+                )
+            if any(
+                candidate["compile"] and candidate["test"]
+                for candidate in sample["evaluation"]
+            ):
+                statistics["num_bugs_with_plausible_candidates"] += 1
+                statistics["bugs_with_plausible_candidates"].append(
+                    sample["identifier"]
+                )
+
+    return statistics
+
+
+def export_patches(samples: list) -> None:
+    """
+    Exports the patches to text files in structured directories.
+    """
+    for sample in tqdm.tqdm(samples):
+        if (
+            sample["generation"] is not None
+            and len(sample["generation"]) > 0
+            and not all(
+                candidate["generation"] is None for candidate in sample["evaluation"]
+            )
+        ):
+            # Write prompt, target diff to file
+            target_diff = difflib.unified_diff(
+                sample["buggy_code"].splitlines(keepends=True),
+                sample["fixed_code"].splitlines(keepends=True),
+                n=max(len(sample["buggy_code"]), len(sample["fixed_code"])),
+            )
+            os.makedirs(f"patches/{sample['identifier']}", exist_ok=True)
+            with open(f"patches/{sample['identifier']}/target.diff", "w") as f:
+                f.writelines(target_diff)
+            with open(f"patches/{sample['identifier']}/prompt.txt", "w") as f:
+                f.write(sample["prompt"])
+
+            for i, candidate in enumerate(sample["evaluation"]):
+                if candidate["generation"] is not None:
+                    # Compute diff between generated code and buggy code
+                    diff = difflib.unified_diff(
+                        sample["buggy_code"].splitlines(keepends=True),
+                        candidate["generation"].splitlines(keepends=True),
+                        n=max(len(sample["buggy_code"]), len(candidate["generation"])),
+                    )
+                    if exact_match(candidate):
+                        sub_dir = "exact_match"
+                    elif plausible(candidate):
+                        sub_dir = "plausible"
+                    elif compilable(candidate):
+                        sub_dir = "compilable"
+                    else:
+                        sub_dir = "non_compilable"
+
+                    os.makedirs(
+                        f"patches/{sample['identifier']}/{sub_dir}", exist_ok=True
+                    )
+                    with open(
+                        f"patches/{sample['identifier']}/{sub_dir}/{i}.diff", "w"
+                    ) as f:
+                        f.writelines(diff)
+
+
 def entry_point(
     benchmark: str,
     samples_path: str,
     strategy: str = "replace",
     n_workers: int = 4,
+    correctness: bool = True,
     **kwargs,
 ):
     """
     Evaluates the candidate patches given the samples,
     and writes the results to f"evaluation_{benchmark}_{prompt_strategy}_{model_name}.jsonl.gz"
-    """
-    results = []
 
+    There are several modes to run this script:
+        - Correctness: correctness=True (default) evaluates the candidate patches for correctness with the given strategy.
+        - Statistics: statistics=True computes statistics over the evaluation.
+        - Export: export=True exports the patches to text files in structured directories.
+    """
     # Get the benchmark, check if it exists, and initialize it
+    prompt_strategy = samples_path.split("_")[2].split(".")[0]
+    model_name = samples_path.split("_")[3].split(".")[0]
     benchmark_obj = get_benchmark(benchmark)
     if benchmark_obj is None:
         raise ValueError(f"Unknown benchmark {benchmark}")
     benchmark_obj.initialize()
 
-    with ThreadPoolExecutor(max_workers=n_workers) as executor:
-        futures = []
+    # Read the samples
+    logging.info("Reading samples...")
+    samples = list(stream_jsonl(samples_path))
 
-        logging.info("Reading samples...")
-        for sample in tqdm.tqdm(stream_jsonl(samples_path)):
-            bug = benchmark_obj.get_bug(sample["identifier"])
-            if bug is None:
-                raise ValueError(f"Unknown bug {sample['identifier']}")
-            futures.append(
-                executor.submit(evaluate_candidate, bug, sample, strategy, **kwargs)
-            )
+    # Correctness evaluation
+    if correctness:
+        with ThreadPoolExecutor(max_workers=n_workers) as executor:
+            futures = []
+            for sample in tqdm.tqdm(samples):
+                bug = benchmark_obj.get_bug(sample["identifier"])
+                if bug is None:
+                    raise ValueError(f"Unknown bug {sample['identifier']}")
+                futures.append(
+                    executor.submit(evaluate_candidate, bug, sample, strategy, **kwargs)
+                )
 
-        logging.info("Generating candidates...")
-        for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
-            results.append(future.result())
+            logging.info("Evaluating candidates...")
+            results = []
+            for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+                results.append(future.result())
+            samples = results
+
+    # Compute statistics over the evaluation
+    if "statistics" in kwargs and kwargs["statistics"]:
+        statistics = compute_statistics(samples)
+        # Write statistics to pretty printed json (not gzip) file
+        with open(
+            f"statistics_{benchmark}_{prompt_strategy}_{model_name}.json", "w"
+        ) as f:
+            json.dump(statistics, f, indent=4)
+
+    # Export patches to text files in structured directories
+    if "export" in kwargs and kwargs["export"]:
+        export_patches(samples)
 
     # Write results to jsonl file
-    benchmark = samples_path.split("_")[1]
-    prompt_strategy = samples_path.split("_")[2].split(".")[0]
-    model_name = samples_path.split("_")[3].split(".")[0]
     write_jsonl(
-        f"evaluation_{benchmark}_{prompt_strategy}_{model_name}.jsonl.gz", results
+        f"evaluation_{benchmark}_{prompt_strategy}_{model_name}.jsonl.gz", samples
     )
 
 
