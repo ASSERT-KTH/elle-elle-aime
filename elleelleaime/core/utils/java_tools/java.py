017a635d9cdd94c6665e9c47032a9baf11d940bb
André Silva <andre15andre@hotmail.com>
2023-10-26 11:53:50 +0200

    clean up fix

---
+++ elleelleaime/core/utils/java_tools/java.py
@@ -0,0 +1,193 @@
+from typing import Optional, Tuple, List
+from unidiff import PatchSet
+from uuid import uuid4
+import os, tempfile, shutil, difflib
+
+from elleelleaime.core.benchmarks.bug import Bug
+from elleelleaime.core.utils.java_tools.java_lang import load_origin_code_node
+
+
+def find_code(file_path: str, line_numbers: List[int]) -> str:
+    """
+    Finds the code corresponding to the given line numbers in the given file.
+    """
+    code = ""
+    with open(file_path, "r", encoding="ISO-8859-1") as file:
+        for idx, line in enumerate(file.readlines()):
+            if idx + 1 in line_numbers:
+                code += line
+    return code
+
+
+def compute_diff(buggy_code: str, fixed_code: str) -> List[str]:
+    """
+    Computes the diff between the buggy and fixed code.
+    """
+    return list(
+        difflib.unified_diff(
+            buggy_code.splitlines(keepends=True),
+            fixed_code.splitlines(keepends=True),
+            n=max(len(buggy_code), len(fixed_code)),
+        )
+    )
+
+
+# Check if the computed diff is equivalent to the original diff
+def assert_same_diff(original_diff: PatchSet, function_diff: List[str]) -> bool:
+    """
+    Checks if the computed diff is equivalent to the original diff
+    """
+    original_source = ""
+    original_target = ""
+    original_added_lines = []
+    original_removed_lines = []
+    # Get the original changed lines
+    # TODO: this diff is inverted, i.e. the target file is the buggy file
+    for file in original_diff:
+        for hunk in file:
+            for line in hunk:
+                if line.is_added:
+                    original_removed_lines.append(line.value.strip())
+                    original_source += line.value
+                elif line.is_removed:
+                    original_added_lines.append(line.value.strip())
+                    original_target += line.value
+                elif line.is_context:
+                    original_source += line.value
+                    original_target += line.value
+    # Get the new changed lines
+    new_source = ""
+    new_target = ""
+    new_added_lines = []
+    new_removed_lines = []
+    for line in function_diff:
+        if any(line.startswith(x) for x in ["---", "+++", "@@"]):
+            continue
+        elif line.startswith("+"):
+            new_added_lines.append(line[1:].strip())
+            new_target += line[1:]
+        elif line.startswith("-"):
+            new_removed_lines.append(line[1:].strip())
+            new_source += line[1:]
+        else:
+            new_source += line[1:]
+            new_target += line[1:]
+    # Check that all the
+    if (
+        any([line not in original_source for line in new_removed_lines])
+        or any([line not in original_target for line in new_added_lines])
+        or any([line not in new_source for line in original_removed_lines])
+        or any([line not in new_target for line in original_added_lines])
+    ):
+        return False
+    return True
+
+
+def extract_single_function(bug: Bug) -> Optional[Tuple[str, str]]:
+    """
+    Extracts the buggy and fixed code of single-function bugs.
+    Returns None is bug is not single-function
+
+    Args:
+        bug (Bug): THe bug to extract the code from
+
+    Returns:
+        Optional[Tuple[str, str]]: None if the bug is not single-function, otherwise a tuple of the form (buggy_code, fixed_code)
+    """
+    buggy_path = os.path.join(
+        tempfile.gettempdir(), "elleelleaime", bug.get_identifier(), str(uuid4())
+    )
+    fixed_path = os.path.join(
+        tempfile.gettempdir(), "elleelleaime", bug.get_identifier(), str(uuid4())
+    )
+
+    try:
+        # Checkout the buggy and fixed versions of the bug
+        bug.checkout(buggy_path, fixed=False)
+        bug.checkout(fixed_path, fixed=True)
+
+        # Note: this diff is inverted, i.e. the target file is the buggy file
+        diff = PatchSet(bug.get_ground_truth())
+
+        buggy_file_path = os.path.join(
+            buggy_path,
+            diff[0].target_file[2:]
+            if diff[0].target_file.startswith("b/")
+            else diff[0].target_file,
+        )
+        fixed_file_path = os.path.join(
+            fixed_path,
+            diff[0].source_file[2:]
+            if diff[0].source_file.startswith("a/")
+            else diff[0].source_file,
+        )
+
+        # Find the methods of each hunk
+        buggy_methods = []
+        fixed_methods = []
+        allowed_node_types = ["MethodDeclaration", "ConstructorDeclaration"]
+        for hunk in diff[0]:
+            buggy_methods.append(
+                load_origin_code_node(
+                    buggy_file_path,
+                    [x.target_line_no for x in hunk.target_lines()],
+                    allowed_node_types,
+                )[0]
+            )
+            fixed_methods.append(
+                load_origin_code_node(
+                    fixed_file_path,
+                    [x.source_line_no for x in hunk.source_lines()],
+                    allowed_node_types,
+                )[0]
+            )
+
+        # Verify that all hunk make changes in the same method
+        buggy_method = buggy_methods[0]
+        fixed_method = fixed_methods[0]
+        for buggy_method, fixed_method in zip(buggy_methods, fixed_methods):
+            if buggy_method != buggy_methods[0] or fixed_method != fixed_methods[0]:
+                return None
+
+        # Get the buggy and fixed code with line numbers
+        # If the ast nodes are not of the correct type, then we have a whole-function removal/addition
+        buggy_code = (
+            find_code(
+                buggy_file_path,
+                [i for i in range(buggy_method.start_pos, buggy_method.end_pos + 1)],
+            )
+            if buggy_method is not None
+            else ""
+        )
+        fixed_code = (
+            find_code(
+                fixed_file_path,
+                [i for i in range(fixed_method.start_pos, fixed_method.end_pos + 1)],
+            )
+            if fixed_method is not None
+            else ""
+        )
+
+        # HACK: sometimes we are not able to properly retrieve the code at the function-level
+        # This happens in cases suchas Closure-46 where a whole function is removed
+        # To detected and circumvent such cases, we check that the function_diff is equivalent to the original diff
+        # If the diffs are not equivalent, we try to fix the function diff by setting the fixed_code and buggy_code to empty
+        # If on of these works we assume it as correct (since the diff is now equivalent to the original one)
+        fdiff = compute_diff(buggy_code, fixed_code)
+        if not assert_same_diff(diff, fdiff):
+            fdiff = compute_diff(buggy_code, "")
+            if assert_same_diff(diff, fdiff):
+                fixed_code = ""
+            else:
+                fdiff = compute_diff("", fixed_code)
+                if assert_same_diff(diff, fdiff):
+                    buggy_code = ""
+                else:
+                    return buggy_code, fixed_code
+
+        return buggy_code, fixed_code
+
+    finally:
+        # Remove the checked-out bugs
+        shutil.rmtree(buggy_path, ignore_errors=True)
+        shutil.rmtree(fixed_path, ignore_errors=True)
--- elleelleaime/sample/prompting/strategies/zero_shot_cloze.py
+++ elleelleaime/sample/prompting/strategies/zero_shot_cloze.py
@@ -1,11 +1,13 @@
 from typing import Optional, Tuple, List
 from unidiff import PatchSet
-from uuid import uuid4
-import re, os, tempfile, shutil, difflib
+import re
 
-from ..strategy import PromptingStrategy
+from elleelleaime.sample.prompting.strategy import PromptingStrategy
 from elleelleaime.core.benchmarks.bug import Bug
-from elleelleaime.core.utils.java_tools.java_lang import load_origin_code_node
+from elleelleaime.core.utils.java_tools.java import (
+    extract_single_function,
+    compute_diff,
+)
 
 
 class ZeroShotClozePrompting(PromptingStrategy):
@@ -57,17 +59,6 @@ class ZeroShotClozePrompting(PromptingStrategy):
         # Build the masking prompt
         return leading_spaces + mask_token
 
-    def find_code(self, file_path: str, line_numbers: List[int]) -> str:
-        """
-        Finds the code corresponding to the given line numbers in the given file.
-        """
-        code = ""
-        with open(file_path, "r", encoding="ISO-8859-1") as file:
-            for idx, line in enumerate(file.readlines()):
-                if idx + 1 in line_numbers:
-                    code += line
-        return code
-
     def cloze_prompt(
         self, bug: Bug
     ) -> Tuple[Optional[str], Optional[str], Optional[str]]:
@@ -79,184 +70,54 @@ class ZeroShotClozePrompting(PromptingStrategy):
         Returns:
             Tuple: A tuple of the form (buggy_code, fixed_code, prompt).
         """
-        buggy_path = os.path.join(
-            tempfile.gettempdir(), "elleelleaime", bug.get_identifier(), str(uuid4())
-        )
-        fixed_path = os.path.join(
-            tempfile.gettempdir(), "elleelleaime", bug.get_identifier(), str(uuid4())
-        )
+        result = extract_single_function(bug)
 
-        try:
-            # Note: this diff is inverted, i.e. the target file is the buggy file
-            diff = PatchSet(bug.get_ground_truth())
+        if result is None:
+            return None, None, None
 
-            bug.checkout(buggy_path, fixed=False)
-            bug.checkout(fixed_path, fixed=True)
+        fdiff = compute_diff(*result)
 
-            buggy_file_path = os.path.join(
-                buggy_path,
-                diff[0].target_file[2:]
-                if diff[0].target_file.startswith("b/")
-                else diff[0].target_file,
-            )
-            fixed_file_path = os.path.join(
-                fixed_path,
-                diff[0].source_file[2:]
-                if diff[0].source_file.startswith("a/")
-                else diff[0].source_file,
-            )
-
-            # Find the methods of each hunk
-            buggy_methods = []
-            fixed_methods = []
-            allowed_node_types = ["MethodDeclaration", "ConstructorDeclaration"]
-            for hunk in diff[0]:
-                buggy_methods.append(
-                    load_origin_code_node(
-                        buggy_file_path,
-                        [x.target_line_no for x in hunk.target_lines()],
-                        allowed_node_types,
-                    )[0]
-                )
-                fixed_methods.append(
-                    load_origin_code_node(
-                        fixed_file_path,
-                        [x.source_line_no for x in hunk.source_lines()],
-                        allowed_node_types,
-                    )[0]
-                )
-
-            # Verify that all hunk make changes in the same method
-            buggy_method = buggy_methods[0]
-            fixed_method = fixed_methods[0]
-            for buggy_method, fixed_method in zip(buggy_methods, fixed_methods):
-                if buggy_method != buggy_methods[0] or fixed_method != fixed_methods[0]:
-                    return None, None, None
-
-            # Get the buggy and fixed code with line numbers
-            # If the ast nodes are not of the correct type, then we have a whole-function removal/addition
-            buggy_code = (
-                self.find_code(
-                    buggy_file_path,
-                    [
-                        i
-                        for i in range(buggy_method.start_pos, buggy_method.end_pos + 1)
-                    ],
-                )
-                if buggy_method is not None
-                else ""
-            )
-            fixed_code = (
-                self.find_code(
-                    fixed_file_path,
-                    [
-                        i
-                        for i in range(fixed_method.start_pos, fixed_method.end_pos + 1)
-                    ],
-                )
-                if fixed_method is not None
-                else ""
-            )
-
-            # Compute the function diff
-            def compute_function_diff(buggy_code: str, fixed_code: str) -> List[str]:
-                return list(
-                    difflib.unified_diff(
-                        buggy_code.splitlines(keepends=True),
-                        fixed_code.splitlines(keepends=True),
-                        n=max(len(buggy_code), len(fixed_code)),
-                    )
-                )
-
-            # Check if the computed diff is equivalent to the original diff
-            def assert_same_diff(
-                original_diff: PatchSet, function_diff: List[str]
-            ) -> bool:
-                original_changed_lines = []
-                for file in original_diff:
-                    for hunk in file:
-                        for line in hunk:
-                            if not line.is_context:
-                                original_changed_lines.append(line.value)
-                for line in function_diff:
-                    if any(line.startswith(x) for x in ["---", "+++", "@@"]):
-                        continue
-                    if any(line.startswith(x) for x in ["+", "-"]):
-                        if (
-                            line[1:] not in original_changed_lines
-                            and line[1:].strip() != ""
-                        ):
-                            return False
-                        elif line[1:].strip() != "":
-                            original_changed_lines.remove(line[1:])
-                if len(original_changed_lines) > 0:
-                    for line in original_changed_lines:
-                        return False
-                return True
-
-            # HACK: sometimes we are not able to properly retrieve the code at the function-level
-            # This happens in cases suchas Closure-46 where a whole function is removed
-            # To detected and circumvent such cases, we check that the function_diff is equivalent to the original diff
-            # If the diffs are not equivalent, we try to fix the function diff by setting the fixed_code and buggy_code to empty
-            # If on of these works we assume it as correct (since the diff is now equivalent to the original one)
-            fdiff = compute_function_diff(buggy_code, fixed_code)
-            if not assert_same_diff(diff, fdiff):
-                fdiff = compute_function_diff(buggy_code, "")
-                if assert_same_diff(diff, fdiff):
-                    fixed_code = ""
-                else:
-                    fdiff = compute_function_diff("", fixed_code)
-                    if assert_same_diff(diff, fdiff):
-                        buggy_code = ""
-                    else:
-                        return buggy_code, fixed_code, None
-
-            # Iterate over both the buggy and fixed code to generate the prompt
-            prompt = ""
-            mask_id = 0
-            i = 0
-            while i < len(fdiff):
-                # Ignore garbage
-                if any(fdiff[i].startswith(x) for x in ["---", "+++", "@@"]):
-                    i += 1
-                # Add a mask token in added/removed chunk of code
-                elif any(fdiff[i].startswith(x) for x in ["+", "-"]):
-                    # If we keep the buggy code we add a first line signaling it and then the first buggy line
+        # Iterate over both the buggy and fixed code to generate the prompt
+        prompt = ""
+        mask_id = 0
+        i = 0
+        while i < len(fdiff):
+            # Ignore garbage
+            if any(fdiff[i].startswith(x) for x in ["---", "+++", "@@"]):
+                i += 1
+            # Add a mask token in added/removed chunk of code
+            elif any(fdiff[i].startswith(x) for x in ["+", "-"]):
+                # If we keep the buggy code we add a first line signaling it and then the first buggy line
+                if self.keep_buggy_code and fdiff[i].startswith("-"):
+                    prompt += "// buggy code\n//" + fdiff[i][1:]
+                # We generate the mask token with the leading spaces of the first buggy line
+                mask_token = self.generate_masking_prompt(fdiff[i][1:], mask_id)
+                i += 1
+                # Skip over the remainder of the added/removed chunk
+                while i < len(fdiff) and any(
+                    fdiff[i].startswith(x) for x in ["+", "-"]
+                ):
+                    # Keep buggy lines if the option is true
                     if self.keep_buggy_code and fdiff[i].startswith("-"):
-                        prompt += "// buggy code\n//" + fdiff[i][1:]
-                    # We generate the mask token with the leading spaces of the first buggy line
-                    mask_token = self.generate_masking_prompt(fdiff[i][1:], mask_id)
+                        prompt += "//" + fdiff[i][1:]
                     i += 1
-                    # Skip over the remainder of the added/removed chunk
-                    while i < len(fdiff) and any(
-                        fdiff[i].startswith(x) for x in ["+", "-"]
-                    ):
-                        # Keep buggy lines if the option is true
-                        if self.keep_buggy_code and fdiff[i].startswith("-"):
-                            prompt += "//" + fdiff[i][1:]
-                        i += 1
-                    # Add the mask token after all buggy lines have been processed
-                    prompt += f"{mask_token}\n"
-                    mask_id += 1
-                # Include unchanged lines
-                else:
-                    prompt += fdiff[i][1:]
-                    i += 1
+                # Add the mask token after all buggy lines have been processed
+                prompt += f"{mask_token}\n"
+                mask_id += 1
+            # Include unchanged lines
+            else:
+                prompt += fdiff[i][1:]
+                i += 1
 
-            # Add extra mask token (e.g. Incoder recommends this in Section 2.2 of their paper)
-            if self.extra_mask_token:
-                prompt += f"{self.generate_masking_prompt('', mask_id)}\n"
+        # Add extra mask token (e.g. Incoder recommends this in Section 2.2 of their paper)
+        if self.extra_mask_token:
+            prompt += f"{self.generate_masking_prompt('', mask_id)}\n"
 
-            # Deal with whole-function addition/removal
-            if prompt == "":
-                prompt = f"{self.generate_masking_prompt('', 0)}"
+        # Deal with whole-function addition/removal
+        if prompt == "":
+            prompt = f"{self.generate_masking_prompt('', 0)}"
 
-            return buggy_code, fixed_code, prompt
-
-        finally:
-            # Remove the checked-out bugs
-            shutil.rmtree(buggy_path, ignore_errors=True)
-            shutil.rmtree(fixed_path, ignore_errors=True)
+        return result[0], result[0], prompt
 
     def prompt(self, bug: Bug) -> Tuple[Optional[str], Optional[str], Optional[str]]:
         """
