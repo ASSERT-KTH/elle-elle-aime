99dde55c249c8632f1c4435c5d2c437e781a9bc1
André Silva <andre15andre@hotmail.com>
2023-11-03 16:49:24 +0100

    fix tty

--- setup.sh
+++ setup.sh
@@ -16,5 +16,5 @@ docker build -t ghrb_framework --load .;
 docker ps -aq --filter "name=ghrb_framework" | grep -q . && docker stop ghrb_framework && docker rm ghrb_framework;
 cd ..;
 docker run -dt --name ghrb_framework -v $(pwd):/root/framework ghrb_framework:latest;
-docker exec -it --workdir /root/framework/ ghrb_framework bash -c "chmod +x cli.py";
-docker exec -it --workdir /root/framework/debug/ ghrb_framework bash -c "python3.9 collector.py";
+docker exec --workdir /root/framework/ ghrb_framework bash -c "chmod +x cli.py";
+docker exec --workdir /root/framework/debug/ ghrb_framework bash -c "python3.9 collector.py";
