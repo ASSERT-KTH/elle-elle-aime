c7dbb6a80e0570f8bc38a9aa32362f70b3b15869
André Silva <andre15andre@hotmail.com>
2024-02-14 16:30:40 +0100

    add execution

---
+++ elleelleaime/evaluate/strategies/mufin/replace.py
@@ -0,0 +1,86 @@
+from typing import Optional, List
+from unidiff import PatchSet
+from uuid import uuid4
+import os, tempfile, shutil, logging
+
+from ..strategy import PatchEvaluationStrategy
+from elleelleaime.core.benchmarks.bug import Bug
+
+
+class MufinReplaceEvaluationStrategy(PatchEvaluationStrategy):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    def _evaluate_impl(self, bug: Bug, sample: dict) -> Optional[List[dict]]:
+        """
+        Returns the evaluation for the given bug and sample.
+
+        :param bug: The bug to generate the prompt for.
+        :param sample: The sample to evaluate.
+        """
+        evaluation = []
+
+        for generation in sample["generation"]:
+            # If the generation is None, we skip the evaluation
+            if generation is None:
+                evaluation.append(
+                    {
+                        "generation": None,
+                        "compile": False,
+                        "test": False,
+                    }
+                )
+                continue
+
+            buggy_path = os.path.join(
+                tempfile.gettempdir(),
+                "elleelleaime",
+                bug.get_identifier(),
+                str(uuid4()),
+            )
+
+            result = {
+                "generation": generation,
+                "compile": False,
+                "test": False,
+            }
+
+            try:
+                # Checkout the buggy code
+                bug.checkout(buggy_path, fixed=False)
+
+                # Locate and load the buggy file
+                file_path = sample["file_path"]
+                with open(file_path, "r", encoding="ISO-8859-1") as f:
+                    fixed_code = f.read()
+
+                # Check that fixed code exists
+                if sample["fixed_code"] not in fixed_code:
+                    logging.error(
+                        f"Could not find buggy code in {file_path} for {sample['identifier']}"
+                    )
+                    break
+
+                # Get the fixed and candidate code
+                candidate_code = fixed_code.replace(sample["buggy_code"], generation)
+
+                # Compute plausible match
+                # Write the buggy code to the file
+                with open(
+                    file_path,
+                    "w",
+                    encoding="ISO-8859-1",
+                    errors="replace",
+                ) as f:
+                    f.write(candidate_code)
+                # Evaluate the buggy code
+                compilation_result = bug.compile(buggy_path)
+                result["compile"] = compilation_result.is_passing()
+                if result["compile"]:
+                    test_result = bug.test(buggy_path)
+                    result["test"] = test_result.is_passing()
+                evaluation.append(result)
+            finally:
+                shutil.rmtree(buggy_path)
+
+        return evaluation
---
+++ evaluate_breaker_samples.py
@@ -0,0 +1,101 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from elleelleaime.core.utils.benchmarks import get_benchmark
+from elleelleaime.core.benchmarks.bug import Bug
+from elleelleaime.core.utils.jsonl import stream_jsonl, write_jsonl
+from elleelleaime.evaluate.strategies.registry import PatchEvaluationStrategyRegistry
+
+import fire
+import sys
+import tqdm
+import logging
+import os
+
+
+def evaluate_candidate(bug: Bug, sample: dict, strategy: str, **kwargs) -> dict:
+    """
+    Evaluates the candidate patch for the given sample.
+    """
+
+    evaluation_strategy = PatchEvaluationStrategyRegistry(**kwargs).get_evaluation(
+        strategy
+    )
+    evaluation = evaluation_strategy.evaluate(bug, sample)
+    sample["evaluation"] = evaluation
+
+    return sample
+
+
+def test(evaluation: dict) -> bool:
+    """
+    Returns True if the evaluation is plausible.
+    """
+    return evaluation["compile"] and evaluation["test"]
+
+
+def compilable(evaluation: dict) -> bool:
+    """
+    Returns True if the evaluation is compilable.
+    """
+    return evaluation["compile"]
+
+
+def entry_point(
+    benchmark: str,
+    samples_path: str,
+    strategy: str = "mufin-replace",
+    n_workers: int = 4,
+    correctness: bool = True,
+    **kwargs,
+):
+    """
+    Evaluates the candidate patches given the samples,
+    and writes the results to f"evaluation_{benchmark}_{prompt_strategy}_{model_name}.jsonl.gz"
+    """
+    # Get the benchmark, check if it exists, and initialize it
+    samples_file_name = os.path.basename(samples_path)
+    dir_path = os.path.dirname(samples_path)
+    prompt_strategy = samples_file_name.split("_")[2].split(".")[0]
+    model_name = samples_file_name.split("_")[3].split(".")[0]
+    benchmark_obj = get_benchmark(benchmark)
+    if benchmark_obj is None:
+        raise ValueError(f"Unknown benchmark {benchmark}")
+    benchmark_obj.initialize()
+
+    # Read the samples
+    logging.info("Reading samples...")
+    samples = list(stream_jsonl(samples_path))
+
+    # Correctness evaluation
+    if correctness:
+        with ThreadPoolExecutor(max_workers=n_workers) as executor:
+            futures = []
+            for sample in tqdm.tqdm(samples):
+                bug = benchmark_obj.get_bug(sample["identifier"])
+                if bug is None:
+                    raise ValueError(f"Unknown bug {sample['identifier']}")
+                futures.append(
+                    executor.submit(evaluate_candidate, bug, sample, strategy, **kwargs)
+                )
+
+            logging.info("Evaluating candidates...")
+            results = []
+            for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+                results.append(future.result())
+            samples = results
+
+    # Write results to jsonl file
+    write_jsonl(
+        os.path.join(
+            dir_path, f"evaluation_{benchmark}_{prompt_strategy}_{model_name}.jsonl.gz"
+        ),
+        samples,
+    )
+
+
+def main():
+    logging.getLogger().setLevel(logging.INFO)
+    fire.Fire(entry_point)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
