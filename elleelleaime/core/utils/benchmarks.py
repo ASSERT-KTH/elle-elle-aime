8ccbadfeb47a99c4cafefea255f038fd40cf6cdc
André Silva <andre15andre@hotmail.com>
2024-02-29 15:12:27 +0100

    implement bears

--- elleelleaime/core/utils/benchmarks.py
+++ elleelleaime/core/utils/benchmarks.py
@@ -2,6 +2,7 @@ from elleelleaime.core.benchmarks.benchmark import Benchmark
 from elleelleaime.core.benchmarks.defects4j.defects4j import Defects4J
 from elleelleaime.core.benchmarks.humanevaljava.humanevaljava import HumanEvalJava
 from elleelleaime.core.benchmarks.quixbugs.quixbugs import QuixBugs
+from elleelleaime.core.benchmarks.bears.bears import Bears
 
 from typing import Optional
 
@@ -9,6 +10,7 @@ benchmarks = {
     "Defects4J": Defects4J,
     "HumanEvalJava": HumanEvalJava,
     "QuixBugs": QuixBugs,
+    "Bears": Bears,
 }
 
 
