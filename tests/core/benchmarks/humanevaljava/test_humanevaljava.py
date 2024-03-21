2e0192a918050dd85ead250cea8fad72bd7205b5
André Silva <andre15andre@hotmail.com>
2024-03-21 16:52:46 +0100

    chore: reduce number of tests in CI

--- tests/core/benchmarks/humanevaljava/test_humanevaljava.py
+++ tests/core/benchmarks/humanevaljava/test_humanevaljava.py
@@ -1,11 +1,12 @@
 from elleelleaime.core.utils.benchmarks import get_benchmark
+from elleelleaime.core.benchmarks.bug import Bug
 
 from pathlib import Path
 import uuid
 import shutil
 import tqdm
+import pytest
 import concurrent.futures
-import logging
 
 
 class TestHumanEvalJava:
@@ -20,7 +21,63 @@ class TestHumanEvalJava:
         assert len(bugs) == 163
         assert len(set([bug.get_identifier() for bug in bugs])) == 163
 
-    def test_checkout(self):
+    def checkout_bug(self, bug: Bug) -> bool:
+        buggy_path = f"/tmp/elleelleaime/{bug.get_identifier()}-buggy-{uuid.uuid4()}"
+        fixed_path = f"/tmp/elleelleaime/{bug.get_identifier()}-fixed-{uuid.uuid4()}"
+
+        try:
+            # Checkout buggy version
+            bug.checkout(buggy_path, fixed=False)
+            # Checkout fixed version
+            bug.checkout(fixed_path, fixed=True)
+
+            # Assert that there are files in the directories
+            if len(list(Path(buggy_path).glob("**/*"))) == 0:
+                return False
+            if len(list(Path(fixed_path).glob("**/*"))) == 0:
+                return False
+
+            # Assert that we can reach the java file
+            if not Path(
+                buggy_path,
+                "src",
+                "main",
+                "java",
+                "humaneval",
+                "buggy",
+                f"{bug.get_identifier()}.java",
+            ).exists():
+                return False
+            if not Path(
+                fixed_path,
+                "src",
+                "main",
+                "java",
+                "humaneval",
+                "buggy",
+                f"{bug.get_identifier()}.java",
+            ).exists():
+                return False
+
+            return True
+        finally:
+            shutil.rmtree(buggy_path, ignore_errors=True)
+            shutil.rmtree(fixed_path, ignore_errors=True)
+
+    def test_checkout_bugs(self):
+        humanevaljava = get_benchmark("humanevaljava")
+        assert humanevaljava is not None
+        humanevaljava.initialize()
+
+        # Run only the first 3 bugs to not take too long
+        bugs = list(humanevaljava.get_bugs())[:3]
+        assert bugs is not None
+
+        for bug in bugs:
+            assert self.checkout_bug(bug), f"Failed checkout for {bug.get_identifier()}"
+
+    @pytest.mark.skip(reason="This test is too slow to run on CI.")
+    def test_checkout_all_bugs(self):
         humanevaljava = get_benchmark("humanevaljava")
         assert humanevaljava is not None
         humanevaljava.initialize()
@@ -29,103 +86,84 @@ class TestHumanEvalJava:
         assert bugs is not None
 
         for bug in bugs:
-            buggy_path = (
-                f"/tmp/elleelleaime/{bug.get_identifier()}-buggy-{uuid.uuid4()}"
-            )
-            fixed_path = (
-                f"/tmp/elleelleaime/{bug.get_identifier()}-fixed-{uuid.uuid4()}"
-            )
-            try:
-                # Checkout buggy version
-                bug.checkout(buggy_path, fixed=False)
-                # Checkout fixed version
-                bug.checkout(fixed_path, fixed=True)
+            assert self.checkout_bug(bug), f"Failed checkout for {bug.get_identifier()}"
 
-                # Assert that there are files in the directories
-                assert len(list(Path(buggy_path).glob("**/*"))) > 0
-                assert len(list(Path(fixed_path).glob("**/*"))) > 0
+    def run_bug(self, bug: Bug) -> bool:
+        buggy_path = f"/tmp/elleelleaime/{bug.get_identifier()}-buggy-{uuid.uuid4()}"
+        fixed_path = f"/tmp/elleelleaime/{bug.get_identifier()}-fixed-{uuid.uuid4()}"
 
-                # Assert that we can reach the java file
-                assert Path(
-                    buggy_path,
-                    "src",
-                    "main",
-                    "java",
-                    "humaneval",
-                    "buggy",
-                    f"{bug.get_identifier()}.java",
-                ).exists()
-                assert Path(
-                    fixed_path,
-                    "src",
-                    "main",
-                    "java",
-                    "humaneval",
-                    "buggy",
-                    f"{bug.get_identifier()}.java",
-                ).exists()
-            finally:
-                shutil.rmtree(buggy_path, ignore_errors=True)
-                shutil.rmtree(fixed_path, ignore_errors=True)
+        try:
+            # Checkout buggy version
+            bug.checkout(buggy_path, fixed=False)
+            # Checkout fixed version
+            bug.checkout(fixed_path, fixed=True)
 
+            # Compile buggy version
+            compile_result = bug.compile(buggy_path)
+            if not compile_result.is_passing():
+                return False
+
+            # Test buggy version
+            test_result = bug.test(buggy_path)
+            if test_result.is_passing():
+                return False
+
+            # Compile fixed version
+            compile_result = bug.compile(fixed_path)
+            if not compile_result.is_passing():
+                return False
+
+            # Test fixed version
+            test_result = bug.test(fixed_path)
+            if not test_result.is_passing():
+                return False
+
+            return True
+        finally:
+            shutil.rmtree(buggy_path, ignore_errors=True)
+            shutil.rmtree(fixed_path, ignore_errors=True)
+
+    def test_run_bugs(self):
+        humanevaljava = get_benchmark("humanevaljava")
+        assert humanevaljava is not None
+        humanevaljava.initialize()
+
+        bugs = list(humanevaljava.get_bugs())
+        assert bugs is not None
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+            futures = []
+            futures_to_bugs = {}
+            for bug in bugs[:3]:  # Only run the first 3 bugs
+                # Submit the bug to be tested as a separate task
+                futures.append(executor.submit(self.run_bug, bug))
+                futures_to_bugs[futures[-1]] = bug
+            # Wait for all tasks to complete
+            for future in tqdm.tqdm(concurrent.futures.as_completed(futures)):
+                result = future.result()
+                assert (
+                    result
+                ), f"Failed run for {futures_to_bugs[future].get_identifier()}"
+
+    @pytest.mark.skip(reason="This test is too slow to run on CI.")
     def test_run_all_bugs(self):
         humanevaljava = get_benchmark("humanevaljava")
         assert humanevaljava is not None
         humanevaljava.initialize()
 
-        # This test takes a while, so we limit to 10 bugs.
-        # Remove the slice to run all bugs.
-        bugs = list(humanevaljava.get_bugs())[:10]
+        bugs = list(humanevaljava.get_bugs())
         assert bugs is not None
-
-        def run_bug(bug):
-            buggy_path = (
-                f"/tmp/elleelleaime/{bug.get_identifier()}-buggy-{uuid.uuid4()}"
-            )
-            fixed_path = (
-                f"/tmp/elleelleaime/{bug.get_identifier()}-fixed-{uuid.uuid4()}"
-            )
-
-            try:
-                # Checkout buggy version
-                bug.checkout(buggy_path, fixed=False)
-                # Checkout fixed version
-                bug.checkout(fixed_path, fixed=True)
-
-                # Compile buggy version
-                compile_result = bug.compile(buggy_path)
-                if not compile_result.is_passing():
-                    return False
-
-                # Test buggy version
-                test_result = bug.test(buggy_path)
-                if test_result.is_passing():
-                    return False
-
-                # Compile fixed version
-                compile_result = bug.compile(fixed_path)
-                if not compile_result.is_passing():
-                    return False
-
-                # Test fixed version
-                test_result = bug.test(fixed_path)
-                if not test_result.is_passing():
-                    return False
-
-                return True
-            finally:
-                shutil.rmtree(buggy_path, ignore_errors=True)
-                shutil.rmtree(fixed_path, ignore_errors=True)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
             futures = []
             futures_to_bugs = {}
             for bug in bugs:
                 # Submit the bug to be tested as a separate task
-                futures.append(executor.submit(run_bug, bug))
+                futures.append(executor.submit(self.run_bug, bug))
                 futures_to_bugs[futures[-1]] = bug
             # Wait for all tasks to complete
             for future in tqdm.tqdm(concurrent.futures.as_completed(futures)):
                 result = future.result()
-                if not result:
-                    print(f"Failed for {futures_to_bugs[future].get_identifier()}")
+                assert (
+                    result
+                ), f"Failed run for {futures_to_bugs[future].get_identifier()}"
--- tests/core/benchmarks/quixbugs/test_quixbugs.py
+++ tests/core/benchmarks/quixbugs/test_quixbugs.py
@@ -1,8 +1,10 @@
 from elleelleaime.core.utils.benchmarks import get_benchmark
+from elleelleaime.core.benchmarks.bug import Bug
 
 from pathlib import Path
-import uuid
 import shutil
+import pytest
+import uuid
 import tqdm
 import concurrent.futures
 
@@ -19,7 +21,34 @@ class TestQuixBugs:
         assert len(bugs) == 40
         assert len(set([bug.get_identifier() for bug in bugs])) == 40
 
-    def test_checkout(self):
+    def checkout_bug(self, bug: Bug) -> bool:
+        buggy_path = f"/tmp/elleelleaime/{bug.get_identifier()}-buggy-{uuid.uuid4()}"
+        fixed_path = f"/tmp/elleelleaime/{bug.get_identifier()}-fixed-{uuid.uuid4()}"
+        try:
+            # Checkout buggy version
+            bug.checkout(buggy_path, fixed=False)
+            # Checkout fixed version
+            bug.checkout(fixed_path, fixed=True)
+
+            # Assert that there are files in the directories
+            assert len(list(Path(buggy_path).glob("**/*"))) > 0
+            assert len(list(Path(fixed_path).glob("**/*"))) > 0
+
+            # Assert that we can reach the java file
+            return (
+                Path(
+                    buggy_path, "java_programs", f"{bug.get_identifier()}.java"
+                ).exists()
+                and Path(
+                    fixed_path, "java_programs", f"{bug.get_identifier()}.java"
+                ).exists()
+            )
+        finally:
+            shutil.rmtree(buggy_path, ignore_errors=True)
+            shutil.rmtree(fixed_path, ignore_errors=True)
+
+    @pytest.mark.skip(reason="This test is too slow to run on CI.")
+    def test_checkout_all_bugs(self):
         quixbugs = get_benchmark("quixbugs")
         assert quixbugs is not None
         quixbugs.initialize()
@@ -28,88 +57,70 @@ class TestQuixBugs:
         assert bugs is not None
 
         for bug in bugs:
-            buggy_path = (
-                f"/tmp/elleelleaime/{bug.get_identifier()}-buggy-{uuid.uuid4()}"
-            )
-            fixed_path = (
-                f"/tmp/elleelleaime/{bug.get_identifier()}-fixed-{uuid.uuid4()}"
-            )
-            try:
-                # Checkout buggy version
-                bug.checkout(buggy_path, fixed=False)
-                # Checkout fixed version
-                bug.checkout(fixed_path, fixed=True)
+            assert self.checkout_bug(bug), f"Failed checkout for {bug.get_identifier()}"
 
-                # Assert that there are files in the directories
-                assert len(list(Path(buggy_path).glob("**/*"))) > 0
-                assert len(list(Path(fixed_path).glob("**/*"))) > 0
+    def test_checkout_bugs(self):
+        quixbugs = get_benchmark("quixbugs")
+        assert quixbugs is not None
+        quixbugs.initialize()
 
-                # Assert that we can reach the java file
-                assert Path(
-                    buggy_path, "java_programs", f"{bug.get_identifier()}.java"
-                ).exists()
-                assert Path(
-                    fixed_path, "java_programs", f"{bug.get_identifier()}.java"
-                ).exists()
-            finally:
-                shutil.rmtree(buggy_path, ignore_errors=True)
-                shutil.rmtree(fixed_path, ignore_errors=True)
+        # We only run 3 bugs to not take too long
+        bugs = list(quixbugs.get_bugs())[:3]
+        assert bugs is not None
 
+        for bug in bugs:
+            assert self.checkout_bug(bug), f"Failed checkout for {bug.get_identifier()}"
+
+    def run_bug(self, bug: Bug) -> bool:
+        buggy_path = f"/tmp/elleelleaime/{bug.get_identifier()}-buggy-{uuid.uuid4()}"
+        fixed_path = f"/tmp/elleelleaime/{bug.get_identifier()}-fixed-{uuid.uuid4()}"
+
+        try:
+            # Checkout buggy version
+            bug.checkout(buggy_path, fixed=False)
+            # Checkout fixed version
+            bug.checkout(fixed_path, fixed=True)
+
+            # Compile buggy version
+            compile_result = bug.compile(buggy_path)
+            if not compile_result.is_passing():
+                return False
+
+            # Test buggy version
+            test_result = bug.test(buggy_path)
+            if test_result.is_passing():
+                return False
+
+            # Compile fixed version
+            compile_result = bug.compile(fixed_path)
+            if not compile_result.is_passing():
+                return False
+
+            # Test fixed version
+            test_result = bug.test(fixed_path)
+            if not test_result.is_passing():
+                return False
+
+            return True
+        finally:
+            shutil.rmtree(buggy_path, ignore_errors=True)
+            shutil.rmtree(fixed_path, ignore_errors=True)
+
+    @pytest.mark.skip(reason="This test is too slow to run on CI.")
     def test_run_all_bugs(self):
         quixbugs = get_benchmark("quixbugs")
         assert quixbugs is not None
         quixbugs.initialize()
 
-        # This test takes a while, so we limit to 10 bugs.
-        # Remove the slice to run all bugs.
-        bugs = list(quixbugs.get_bugs())[:10]
+        bugs = list(quixbugs.get_bugs())
         assert bugs is not None
-
-        def run_bug(bug):
-            buggy_path = (
-                f"/tmp/elleelleaime/{bug.get_identifier()}-buggy-{uuid.uuid4()}"
-            )
-            fixed_path = (
-                f"/tmp/elleelleaime/{bug.get_identifier()}-fixed-{uuid.uuid4()}"
-            )
-
-            try:
-                # Checkout buggy version
-                bug.checkout(buggy_path, fixed=False)
-                # Checkout fixed version
-                bug.checkout(fixed_path, fixed=True)
-
-                # Compile buggy version
-                compile_result = bug.compile(buggy_path)
-                if not compile_result.is_passing():
-                    return False
-
-                # Test buggy version
-                test_result = bug.test(buggy_path)
-                if test_result.is_passing():
-                    return False
-
-                # Compile fixed version
-                compile_result = bug.compile(fixed_path)
-                if not compile_result.is_passing():
-                    return False
-
-                # Test fixed version
-                test_result = bug.test(fixed_path)
-                if not test_result.is_passing():
-                    return False
-
-                return True
-            finally:
-                shutil.rmtree(buggy_path, ignore_errors=True)
-                shutil.rmtree(fixed_path, ignore_errors=True)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
             futures = []
             futures_to_bugs = {}
             for bug in bugs:
                 # Submit the bug to be tested as a separate task
-                futures.append(executor.submit(run_bug, bug))
+                futures.append(executor.submit(self.run_bug, bug))
                 futures_to_bugs[futures[-1]] = bug
             # Wait for all tasks to complete
             for future in tqdm.tqdm(concurrent.futures.as_completed(futures)):
@@ -117,4 +128,28 @@ class TestQuixBugs:
                 if not result:
                     assert (
                         result
-                    ), f"Failed for {futures_to_bugs[future].get_identifier()}"
+                    ), f"Failed run bug for {futures_to_bugs[future].get_identifier()}"
+
+    def test_run_bugs(self):
+        quixbugs = get_benchmark("quixbugs")
+        assert quixbugs is not None
+        quixbugs.initialize()
+
+        # We only run 3 bugs to not take too long
+        bugs = list(quixbugs.get_bugs())[:3]
+        assert bugs is not None
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+            futures = []
+            futures_to_bugs = {}
+            for bug in bugs:
+                # Submit the bug to be tested as a separate task
+                futures.append(executor.submit(self.run_bug, bug))
+                futures_to_bugs[futures[-1]] = bug
+            # Wait for all tasks to complete
+            for future in tqdm.tqdm(concurrent.futures.as_completed(futures)):
+                result = future.result()
+                if not result:
+                    assert (
+                        result
+                    ), f"Failed run bug for {futures_to_bugs[future].get_identifier()}"
