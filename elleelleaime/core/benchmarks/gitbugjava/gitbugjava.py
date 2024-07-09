bf2546ba87a795bc0245e3713956642e20e4415c
André Silva <andre15andre@hotmail.com>
2024-07-09 12:59:08 +0200

    try to fix gitbug-java command

--- elleelleaime/core/benchmarks/gitbugjava/gitbugjava.py
+++ elleelleaime/core/benchmarks/gitbugjava/gitbugjava.py
@@ -16,7 +16,7 @@ class GitBugJava(Benchmark):
     # TODO: add GitBug-Java to benchmarks sub-modules
     def __init__(self, path: Path = Path("benchmarks/gitbug-java").absolute()) -> None:
         super().__init__("gitbugjava", path)
-        self.bin = f"cd {self.path} && poetry env use python3.12 > /dev/null && poetry run {path.joinpath('gitbug-java')}"
+        self.bin = f"cd {self.path} && poetry env use python > /dev/null && poetry run {path.joinpath('gitbug-java')}"
 
     def get_bin(self) -> str:
         return self.bin
