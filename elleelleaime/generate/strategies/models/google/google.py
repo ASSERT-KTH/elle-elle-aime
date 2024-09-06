cf065ea2158a7f2eb48a755327a51133c0349d30
André Silva <andre15andre@hotmail.com>
2024-09-06 11:36:20 +0200

    fix: handle google exceptions and with backoff (#153)
    * fix: handle google exceptions and add backoff
    
    * add backoff

--- elleelleaime/generate/strategies/models/google/google.py
+++ elleelleaime/generate/strategies/models/google/google.py
@@ -1,15 +1,23 @@
+import google.api_core
+import google.api_core.exceptions
 from elleelleaime.generate.strategies.strategy import PatchGenerationStrategy
 
 from dotenv import load_dotenv
 from typing import Any, List
 
 import os
+import tqdm
 import google.generativeai as genai
+import google
+import backoff
+
+import google.api
 
 
 class GoogleModels(PatchGenerationStrategy):
     def __init__(self, model_name: str, **kwargs) -> None:
         self.model_name = model_name
+        self.model = genai.GenerativeModel(self.model_name)
         self.temperature = kwargs.get("temperature", 0.0)
         self.n_samples = kwargs.get("n_samples", 1)
 
@@ -21,18 +29,20 @@ class GoogleModels(PatchGenerationStrategy):
             temperature=self.temperature,
         )
 
+    @backoff.on_exception(backoff.expo, google.api_core.exceptions.ResourceExhausted)
+    def __generate_with_backoff(self, prompt: str) -> dict:
+        completion = self.model.generate_content(
+            prompt, generation_config=self.__get_config()
+        )
+        return completion.to_dict()
+
     def _generate_impl(self, chunk: List[str]) -> Any:
         result = []
 
-        model = genai.GenerativeModel(self.model_name)
-
-        for prompt in chunk:
+        for prompt in tqdm.tqdm(chunk, "Generating patches for prompt..."):
             p_results = []
             for _ in range(self.n_samples):
-                completion = model.generate_content(
-                    prompt, generation_config=self.__get_config()
-                )
-                p_results.append(completion.to_dict())
+                p_results.append(self.__generate_with_backoff(prompt))
             result.append(p_results)
 
         return result
