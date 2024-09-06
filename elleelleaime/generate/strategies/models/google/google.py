f21b8274f978d0d85a67b51693916dac7fed1393
André Silva <andre15andre@hotmail.com>
2024-09-06 10:25:02 +0200

    fix: handle google exceptions and add backoff

--- elleelleaime/generate/strategies/models/google/google.py
+++ elleelleaime/generate/strategies/models/google/google.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 from typing import Any, List
 
 import os
+import tqdm
 import google.generativeai as genai
 
 
 class GoogleModels(PatchGenerationStrategy):
     def __init__(self, model_name: str, **kwargs) -> None:
         self.model_name = model_name
+        self.model = genai.GenerativeModel(self.model_name)
         self.temperature = kwargs.get("temperature", 0.0)
         self.n_samples = kwargs.get("n_samples", 1)
 
@@ -24,12 +26,11 @@ class GoogleModels(PatchGenerationStrategy):
     def _generate_impl(self, chunk: List[str]) -> Any:
         result = []
 
-        model = genai.GenerativeModel(self.model_name)
 
-        for prompt in chunk:
+        for prompt in tqdm.tqdm(chunk, "Generating patches for prompt..."):
             p_results = []
             for _ in range(self.n_samples):
-                completion = model.generate_content(
+                completion = self.model.generate_content(
                     prompt, generation_config=self.__get_config()
                 )
                 p_results.append(completion.to_dict())
