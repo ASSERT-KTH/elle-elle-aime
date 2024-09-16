a410fa0fb0812606f3c2fb7b255e66c42cd26ed6
André Silva <andre15andre@hotmail.com>
2024-09-16 11:12:55 +0200

    add cost

--- elleelleaime/export/cost/strategies/openai.py
+++ elleelleaime/export/cost/strategies/openai.py
@@ -6,16 +6,20 @@ import tqdm
 
 class OpenAICostStrategy(CostStrategy):
 
-    __COST_PER_THOUSAND_TOKENS = {
+    __COST_PER_MILLION_TOKENS = {
         "gpt-4o-2024-08-06": {
-            "prompt": 0.00250,
-            "completion": 0.01000,
-        }
+            "prompt": 2.5,
+            "completion": 10,
+        },
+        "o1-preview-2024-09-12": {
+            "prompt": 15,
+            "completion": 60,
+        },
     }
 
     @staticmethod
     def compute_costs(samples: list, model_name: str) -> Optional[dict]:
-        if model_name not in OpenAICostStrategy.__COST_PER_THOUSAND_TOKENS:
+        if model_name not in OpenAICostStrategy.__COST_PER_MILLION_TOKENS:
             return None
 
         costs = {
@@ -26,22 +30,25 @@ class OpenAICostStrategy(CostStrategy):
 
         for sample in tqdm.tqdm(samples, f"Computing costs for {model_name}..."):
             if sample["generation"]:
-                prompt_token_count = sample["generation"]["usage"]["prompt_tokens"]
-                candidates_token_count = sample["generation"]["usage"][
-                    "completion_tokens"
-                ]
+                if not isinstance(sample["generation"], list):
+                    generation = [sample["generation"]]
+                else:
+                    generation = sample["generation"]
+                for g in generation:
+                    prompt_token_count = g["usage"]["prompt_tokens"]
+                    candidates_token_count = g["usage"]["completion_tokens"]
 
-                prompt_cost = OpenAICostStrategy.__COST_PER_THOUSAND_TOKENS[model_name][
-                    "prompt"
-                ]
-                completion_cost = OpenAICostStrategy.__COST_PER_THOUSAND_TOKENS[
-                    model_name
-                ]["completion"]
+                    prompt_cost = OpenAICostStrategy.__COST_PER_MILLION_TOKENS[
+                        model_name
+                    ]["prompt"]
+                    completion_cost = OpenAICostStrategy.__COST_PER_MILLION_TOKENS[
+                        model_name
+                    ]["completion"]
 
-                costs["prompt_cost"] += prompt_cost * prompt_token_count / 1000
-                costs["completion_cost"] += (
-                    completion_cost * candidates_token_count / 1000
-                )
+                    costs["prompt_cost"] += prompt_cost * prompt_token_count / 1000000
+                    costs["completion_cost"] += (
+                        completion_cost * candidates_token_count / 1000000
+                    )
 
         costs["total_cost"] = costs["prompt_cost"] + costs["completion_cost"]
         return costs
