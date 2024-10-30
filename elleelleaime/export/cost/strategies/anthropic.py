b575cb6c614616ff2586eba5dcdd2772eb3186c5
André Silva <andre15andre@hotmail.com>
2024-10-30 09:56:09 +0100

    add claude-3-5-sonnet-20241022

--- elleelleaime/export/cost/strategies/anthropic.py
+++ elleelleaime/export/cost/strategies/anthropic.py
@@ -12,6 +12,10 @@ class AnthropicCostStrategy(CostStrategy):
             "prompt": 3,
             "completion": 15,
         },
+        "claude-3-5-sonnet-20241022": {
+            "prompt": 3,
+            "completion": 15,
+        },
         "claude-3-haiku-20240307": {
             "prompt": 0.25,
             "completion": 1.25,
