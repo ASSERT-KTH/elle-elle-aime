c21cde9354812a9491a727a6935c690750f9becc
André Silva <andre15andre@hotmail.com>
2024-12-18 12:52:04 +0100

    fix setup?

--- setup.sh
+++ setup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# ## Submodules
-# git submodule init;
-# git submodule update;
+## Submodules
+git submodule init;
+git submodule update;
 
-# ### Java and Maven images
-# docker pull openjdk:11;
-# docker pull maven:3.9.8-eclipse-temurin-8;
+### Java and Maven images
+docker pull openjdk:11;
+docker pull maven:3.9.8-eclipse-temurin-8;
 
-# ### Defects4J image
-# cd benchmarks/defects4j;
-# cpanm --installdeps .;
-# ./init.sh;
-# cd ../..;
+### Defects4J image
+cd benchmarks/defects4j;
+cpanm --installdeps .;
+./init.sh;
+cd ../..;
 
-# ### GitBug-Java
-# cd benchmarks/gitbug-java;
-# chmod +x gitbug-java;
-# poetry install --no-root;
-# # Skip setup if in CI
-# if [ -z "$CI" ]; then
-#  poetry run ./gitbug-java setup;
-# fi
+### GitBug-Java
+cd benchmarks/gitbug-java;
+chmod +x gitbug-java;
+poetry install --no-root;
+# Skip setup if in CI
+if [ -z "$CI" ]; then
+ poetry run ./gitbug-java setup;
+fi
 
 cd benchmarks/run_bug_run;
 tar -xvzf buggy_test_results.tgz;
