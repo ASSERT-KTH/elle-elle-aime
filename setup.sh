bb40987b47a5aa255821440bdadfae1de1f6b25a
André Silva <andre15andre@hotmail.com>
2024-12-18 12:51:34 +0100

    fix setup?

--- setup.sh
+++ setup.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
-## Submodules
-git submodule init;
-git submodule update;
+# ## Submodules
+# git submodule init;
+# git submodule update;
 
-### Java and Maven images
-docker pull openjdk:11;
-docker pull maven:3.9.8-eclipse-temurin-8;
+# ### Java and Maven images
+# docker pull openjdk:11;
+# docker pull maven:3.9.8-eclipse-temurin-8;
 
-### Defects4J image
-cd benchmarks/defects4j;
-cpanm --installdeps .;
-./init.sh;
-cd ../..;
+# ### Defects4J image
+# cd benchmarks/defects4j;
+# cpanm --installdeps .;
+# ./init.sh;
+# cd ../..;
 
-### GitBug-Java
-cd benchmarks/gitbug-java;
-chmod +x gitbug-java;
-poetry install --no-root;
-# Skip setup if in CI
-if [ -z "$CI" ]; then
- poetry run ./gitbug-java setup;
-fi
+# ### GitBug-Java
+# cd benchmarks/gitbug-java;
+# chmod +x gitbug-java;
+# poetry install --no-root;
+# # Skip setup if in CI
+# if [ -z "$CI" ]; then
+#  poetry run ./gitbug-java setup;
+# fi
 
-mkdir -p benchmarks/run_bug_run;
 cd benchmarks/run_bug_run;
-wget https://github.com/giganticode/run_bug_run_data/releases/download/v0.0.1/python_valid0.jsonl.gz;
-wget https://github.com/giganticode/run_bug_run_data/releases/download/v0.0.1/tests_all.jsonl.gz;
-
-gzip -d python_valid0.jsonl.gz;
-gzip -d tests_all.jsonl.gz;
-gzip -d buggy_test_results.tgz;
-tar xvf buggy_test_results.tar;
+tar -xvzf buggy_test_results.tgz;
 cd ../..;
